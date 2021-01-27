173816d17b36577e0be99aefb45bf698fd297521
Ivandeclei <ivandeclei@gmail.com>
2021-01-27 15:39:14 -0300

    tratamento de excessao personalizado e alteracao do service

--- src/main/java/com/example/curso/services/UserService.java
+++ src/main/java/com/example/curso/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.curso.entities.User;
 import com.example.curso.repositories.UserRepository;
+import com.example.curso.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));//nesta expressao lambda esta usando as classes de excessao personalizzada
 	}
 	
 	public User insert(User obj) {
